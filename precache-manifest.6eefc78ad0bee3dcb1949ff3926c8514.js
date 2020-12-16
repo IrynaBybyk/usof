cace8d6ca8272db2cf9db44e84667cb4568e7ef0
Iryna Bybyk <ibybyk@e1r6p1.unit.ua>
2020-12-16 18:47:51 +0200

    Updates

---
+++ precache-manifest.6eefc78ad0bee3dcb1949ff3926c8514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f8042d855996b0366be2dab9500b11",
+    "url": "/usof/index.html"
+  },
+  {
+    "revision": "c29fbaccf9ff2ea57a46",
+    "url": "/usof/static/css/main.723cd43c.chunk.css"
+  },
+  {
+    "revision": "516970b4e8e71d3362a6",
+    "url": "/usof/static/js/2.cf5e0c2f.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/usof/static/js/2.cf5e0c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29fbaccf9ff2ea57a46",
+    "url": "/usof/static/js/main.c13a345d.chunk.js"
+  },
+  {
+    "revision": "0d588a82f861d39e42bc",
+    "url": "/usof/static/js/runtime-main.1bcfb7fe.js"
+  }
+]);
